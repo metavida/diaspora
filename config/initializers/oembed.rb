53968288b48eb1eb0145086c2c024a440f93bcde
Marcos Wright-Kuhns <webmaster@kuhnsfam.com>
2012-03-15 13:58:03 -0700

    Switch Vimeo oEmbed Provider to SSL
    Pull #3014 added support for the Vimeo `OEmbed::Provider` but the Vimeo provider that comes with the ruby-oembed gem returns standard http embed code. Using the un-document https endpoint returns https embed code as requested in #2342
    
    Also added a note about the Cubbies `OEmbed::Provider` requiring a URL scheme to work.

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -7,14 +7,19 @@ require 'uri'
 # note that 'trusted_endpoint_url' is the only information
 # in OEmbed that we can trust. anything else may be spoofed!
 
-OEmbedCubbies = OEmbed::Provider.new("http://cubbi.es/oembed")
+OEmbed::Providers::Cubbies = OEmbed::Provider.new("http://cubbi.es/oembed")
+# This provider won't work without specifying at least one supported URL scheme.
+# e.g. OEmbed::Providers::Cubbies << "http://cubbi.es/*"
+OEmbed::Providers::VimeoSSL = OEmbed::Provider.new("https://vimeo.com/api/oembed.{format}")
+OEmbed::Providers::VimeoSSL << "http://*.vimeo.com/*"
+OEmbed::Providers::VimeoSSL << "https://*.vimeo.com/*"
 
 oembed_provider_list = [
   OEmbed::Providers::Youtube,
-  OEmbed::Providers::Vimeo,
+  OEmbed::Providers::VimeoSSL,
   OEmbed::Providers::Flickr,
   OEmbed::Providers::SoundCloud,
-  OEmbedCubbies
+  OEmbed::Providers::Cubbies,
 ]
 
 SECURE_ENDPOINTS = oembed_provider_list.map do |provider|
